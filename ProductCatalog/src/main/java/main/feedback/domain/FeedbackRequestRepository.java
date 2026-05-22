e52d0aeffc397f6b215bb29e59695ce3a5b786aa
FireZoner <zubbodazdeban@mail.ru>
2026-05-23 00:47:15 +0300

    Связал отправку заявок с SMTP (#9)

--- ProductCatalog/src/main/java/main/feedback/domain/FeedbackRequestRepository.java
+++ ProductCatalog/src/main/java/main/feedback/domain/FeedbackRequestRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import main.user.domain.AppUser;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  *
@@ -16,4 +17,13 @@ import java.util.List;
 public interface FeedbackRequestRepository extends JpaRepository<FeedbackRequest, Long> {
     List<FeedbackRequest> findByUserOrderByCreatedAtDesc(AppUser user);
     List<FeedbackRequest> findByStatusOrderByCreatedAtAsc(FeedbackStatus status);
+    
+    @Query("""
+            select request
+            from FeedbackRequest request
+            left join fetch request.product
+            where request.user = :user
+            order by request.createdAt desc
+            """)
+    List<FeedbackRequest> findHistoryByUser(AppUser user);
 }
